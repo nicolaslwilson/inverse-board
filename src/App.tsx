784b67ebac70d4e56f96a07efa3240ea4be85d9d
nic <nic.wilson@gmail.com>
2023-01-16 12:43:08 -0600

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  collection,
+  doc,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import {
+  FirestoreProvider,
+  useFirestoreDocData,
+  useFirestore,
+  useFirebaseApp,
+  useFirestoreCollectionData,
+} from "reactfire";
+import { Layout, List } from "antd";
+
+const { Header, Footer, Content } = Layout;
+
+function ZetaBoard() {
+  const firestore = useFirestore();
+  const zetaCollection = collection(firestore, "zetaAccountHealth");
+  const accountQuery = query(
+    zetaCollection,
+    orderBy("health", "asc"),
+    limit(10)
+  );
+  const { status, data: accounts } = useFirestoreCollectionData(accountQuery, {
+    idField: "id",
+  });
+
+  if (status === "loading") {
+    return <span>loading...</span>;
+  }
+
+  console.log(accounts);
+  return (
+    <List
+      bordered
+      dataSource={accounts}
+      renderItem={(account) => (
+        <List.Item>
+          {account.publicKey} {account.health}
+        </List.Item>
+      )}
+    />
+  );
+}
 
 function App() {
+  const app = useFirebaseApp();
+  const firestore = getFirestore(app);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirestoreProvider sdk={firestore}>
+      <Header>🐸 Inverse Board</Header>
+      <Content>
+        <ZetaBoard></ZetaBoard>
+      </Content>
+      <Footer></Footer>
+    </FirestoreProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "antd/dist/reset.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { FirebaseAppProvider } from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBs0sl1WEwRAIdMLQ6QXAMjjNrvC4n-jh4",
+  authDomain: "mm-liquid.firebaseapp.com",
+  projectId: "mm-liquid",
+  storageBucket: "mm-liquid.appspot.com",
+  messagingSenderId: "102063149507",
+  appId: "1:102063149507:web:297a84a2627850221f6d76",
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>
 );
 
