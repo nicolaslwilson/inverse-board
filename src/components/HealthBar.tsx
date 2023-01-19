55686d609a3effbe7ce242d494db8eaf6be14fb8
nic <nic.wilson@gmail.com>
2023-01-19 10:30:21 -0600

    fix healthbar

--- src/components/HealthBar.tsx
+++ src/components/HealthBar.tsx
@@ -4,10 +4,13 @@ import { getColorForHealth } from '../utils/gradient';
 export function HealthBar({ health }: { health: number }) {
   const healthPercent = health * 100;
   return (
-    <Progress
-      percent={Math.round(healthPercent * 100) / 100}
-      strokeColor={getColorForHealth(health)}
-      size="small"
-    ></Progress>
+    <div>
+      <Progress
+        percent={Math.round(healthPercent * 100) / 100}
+        strokeColor={getColorForHealth(health)}
+        size="small"
+        style={{ width: '98%' }}
+      ></Progress>
+    </div>
   );
 }
